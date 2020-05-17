b313f3cbae180a8831e9b5bb0a937eeaed6c6668
Venu Madhav <venumadhav888@gmail.com>
2020-05-17 19:55:38 +0530

    Create DeployProperties.py

---
+++ DeployProperties.py
@@ -0,0 +1,8 @@
+ear.name1=test1.ear
+ear.name2=test2.ear
+
+# Targets
+server=AdminServer
+
+#Application path where the deploymenst are there
+applicationpath=/home/weblogic/DOMAIN_HOME/servers/Adminserver/Deployment
