8222e0493e309d749d261a40db9f3e2bcf2925ba
Venu Madhav <venumadhav888@gmail.com>
2020-05-17 19:15:59 +0530

    Create WLSProperties.py

---
+++ WLSProperties.py
@@ -0,0 +1,6 @@
+# Weblogic domain details
+---------------------------------------------------------------------------------------
+wlusr=weblogic
+wlpwd=weblogic123
+wllistenaddress=172.16.56.98
+wllistenport=9512
